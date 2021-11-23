e9ee94d5c9623d96a7fd1fd603903dd01ebeeef5
Wannes Van de Putte <vandeputte.wannes@gmail.com>
2021-11-23 10:05:10 +0100

    Afbeeldingen

--- visualDartCounter.py
+++ visualDartCounter.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python3
 from calibration import calibrate
+from points import point
 
 
 __version__ = "0.1" #Uitlezen van foto
 
 def main():
-    calibrate("./images/dartboard.png")
-    calibrate("./images/dartboard2.png")
+    puntenregio = calibrate("./images/dartboard.png")
+    print("41")
+    print(point("./images/dartboard_41.png", puntenregio))
+    print("57")
+    print(point("./images/dartboard_57.png", puntenregio))
+    print("66")
+    print(point("./images/dartboard_66.png", puntenregio))
+
+
+    puntenregio = calibrate("./images/dartboard2.png")
 
 if __name__ == "__main__":
     main()
